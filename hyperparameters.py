076fce4d30e1869ba38329ef73ad82b60d23a06a
Ronak Mehta <ronakdm@uw.edu>
2021-12-08 14:29:09 -0800

    Making search space smaller.

--- hyperparameters.py
+++ hyperparameters.py
@@ -3,9 +3,9 @@ import random
 search_space = {
     "num_layers": [1, 2, 3, 4],
     "hidden_dim": [4, 8, 16, 64, 128],
-    "learning_rate": [1e-5, 3e-5, 1e-4, 3e-4, 1e-3, 3e-3, 1e-2, 3e-2],
-    "embed_dim0": [4, 8, 16, 32, 64, 128],
-    "embed_dim1": [4, 8, 16, 32, 64, 128],
+    "learning_rate": [3e-3, 1e-2],
+    "embed_dim0": [4, 8, 16],
+    "embed_dim1": [4, 8, 16, 32],
     "epochs": [4, 8, 16, 32],
     "weight_decay": [0.0, 3e-4, 1e-3, 3e-3, 1e-2, 3e-2, 1e-1],
 }
@@ -14,9 +14,9 @@ print(random.sample(search_space["num_layers"], 2))
 
 
 def sample_hyperparameters():
-    num_layers = random.sample(search_space["num_layers"], 1)[0]
+    # num_layers = random.sample(search_space["num_layers"], 1)[0]
     return {
-        "hidden_dims": random.sample(search_space["hidden_dim"], num_layers),
+        "hidden_dims": random.sample(search_space["hidden_dim"], 2),
         "learning_rate": random.sample(search_space["learning_rate"], 1)[0],
         "embed_dim0": random.sample(search_space["embed_dim0"], 1)[0],
         "embed_dim1": random.sample(search_space["embed_dim1"], 1)[0],
